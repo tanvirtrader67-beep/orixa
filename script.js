52ec5684480cc46188f6408a1a916bdaaef25b43
tanvirtrader67-beep <tanvirtrader67@gmail.com>
2026-07-05 19:31:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("startBtn").addEventListener("click", function () {
+    alert("Welcome to ORIXA!");
+
+    // পরে এখানে Login Page-এ যাওয়ার কোড যোগ করবো
+    // window.location.href = "login.html";
+});
